41afb75e239fb45b1d13b8681407b4b198ddcc7e
Ben Mordue <benmordue@gmail.com>
2022-05-15 17:53:13 +0100

    config tweaks for bar chart

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const esModules = ['.*\\.mjs', 'robust-predicates', 'delaunator', 'internmap', 'd3', 'd3-array'].join('|');
 export default {
   displayName: 'ui',
   preset: '../../jest.preset.js',
@@ -13,7 +14,7 @@ export default {
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
