9444174a4994f83f28fe08d080a81ab68f3a97f7
Ben Mordue <benmordue@gmail.com>
2022-05-15 16:57:17 +0100

    nx g @nrwl/angular:component d3-bar-chart

---
+++ packages/ui/src/lib/d3-bar-chart/d3-bar-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-bmordue-d3-bar-chart',
+  templateUrl: './d3-bar-chart.component.html',
+  styleUrls: ['./d3-bar-chart.component.css'],
+})
+export class D3BarChartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- packages/ui/src/lib/ui.module.ts
+++ packages/ui/src/lib/ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { D3BarChartComponent } from './d3-bar-chart/d3-bar-chart.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [D3BarChartComponent],
 })
 export class UiModule {}
